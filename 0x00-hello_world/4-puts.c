dba72998a4ba082861c23e48e1d1da0cd340b2bc
Graceomole <graceomoleye123@gmail.com>
2022-09-08 14:41:57 -0700

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
 
-
-
 /**
-
- * main - Printing
-
+ * main - Entry point
  *
-
- * Return: Always 0 (Successful)
-
+ *Return: Always 0 (Success)
  */
-
 int main(void)
-  
 {
-  
-  puts("\"Programming is like building a multilingual
-
-puzzle");
-  
-  return (0);
-  
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
