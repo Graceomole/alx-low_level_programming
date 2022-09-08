5a260693892cfb0cd816361489855ac55f4f792c
Grace Omoleye <105713462+Graceomole@users.noreply.github.com>
2022-09-08 23:24:58 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,12 @@ int main(void)
         long int c;
         long long int d;
         float e;
-        
+
         printf("Size of a char: %d byte(s)\n", sizeof(a));
         printf("Size of an int: %d byte(s)\n", sizeof(b));
         printf("Size of a long int: %d byte(s)\n", sizeof(c));
         printf("Size of a long long int: %d byte(s)\n", sizeof(d));
         printf("Size of a float: %d byte(s)\n", sizeof(e));
         return (0);
+
 }
