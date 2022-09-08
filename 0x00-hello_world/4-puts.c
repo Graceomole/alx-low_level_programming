d743f599ba0a8e7949d013f82dad5056a715a13b
Grace Omoleye <105713462+Graceomole@users.noreply.github.com>
2022-09-08 22:22:24 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
 
-
-
 /**
-
  * main - Printing
-
  *
-
  * Return: Always 0 (Successful)
-
  */
-
 int main(void)
-  
 {
-  
   puts("\"Programming is like building a multilingual
-
-puzzle");
-  
+  puzzle");
   return (0);
-  
 }
