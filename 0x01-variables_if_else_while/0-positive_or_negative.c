9d4c06470cb91af6208b453a59e79d5f851cf5c2
Grace Omoleye <105713462+Graceomole@users.noreply.github.com>
2022-09-10 00:29:20 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-  printf("%d is positive", n);
+printf("%d is positive", n);
 }
 else if (n == 0)
 {
-  printf("%d is zero", n);
+printf("%d is zero", n);
 }
 else if (n < 0)
 {
