4e29e229d91773c69782ef607c8fd0dc2f62cad9
Grace Omoleye <105713462+Graceomole@users.noreply.github.com>
2022-09-10 02:01:01 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ n = rand() - RAND_MAX / 2;
 x = n % 10;
 if (n > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, x);
+printf("Last digit of %d is %d and is 0\n", n, x);
 }
 else if (n == 0)
 {
