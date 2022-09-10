095e22e51d220cbb38a7e6c5824c05d1184af9ed
Grace Omoleye <105713462+Graceomole@users.noreply.github.com>
2022-09-10 03:32:42 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
  * Description: Check that prints letters below 10
  * Return: 0
  */
+
 int main(void)
 {
-  int x;
-
-  for (x = 0; x < 10; x++)
-    {
-      putchar(x + '0');
-    }
-  putchar('\n');
-  return (0);
+int x;
+for (x = 0; x < 10; x++)
+{
+putchar(x + '0');
+}
+putchar('\n');
+return (0);
 }
