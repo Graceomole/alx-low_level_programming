21566a22690c1b0ff424dbdcc55765ec03d8d009
Graceomole <graceomoleye123@gmail.com>
2022-09-09 19:47:40 -0700

    C codes

---
+++ 0x01-variables_if_else_while/9-prints_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Check that prints num 0 to 9
+ *
+ *Return:Always 0 (Succesful)
+ */
+int main(void)
+{
+  int c;
+  for (c = '0'; c <= '9' ; c++)
+    {
+      putchar(c);
+      if (c != '9')
+	{
+	  putchar(',');
+	  putchar(' ');
+	}
+    }
+  putchar('\n');
+  return (0);
+}
