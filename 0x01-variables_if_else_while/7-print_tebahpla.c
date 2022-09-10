1426cf9a69dd0ddd161c204235e2313dccc8de1b
Graceomole <graceomoleye123@gmail.com>
2022-09-09 19:36:57 -0700

    C codes

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Check that prints reversed alphabet in lowercase in reverse
+ * Return: 0
+ */
+int main(void)
+{
+  int x;
+
+  for (x = 'z'; x >= 'a'; x--)
+    {
+      putchar(x);
+    }
+  putchar('\n');
+  return (0);
+}
