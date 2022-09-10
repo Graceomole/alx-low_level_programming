709de12780a4998fdc2d237025e31576d79c414f
Graceomole <graceomoleye123@gmail.com>
2022-09-09 19:41:56 -0700

    C codes

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - Check that prints base 16 all in lowercase alphabates
+ *
+ *Return:Always 0 (Succesful)
+ */
+int main(void)
+{
+  char c;
+  for (c = '0'; c <= '9' ; c++)
+    {
+      putchar(c);
+    }
+  for (c = 'a'; c <= 'f' ; c++)
+    {
+      putchar(c);
+    }
+  putchar('\n');
+  return (0);
+}
