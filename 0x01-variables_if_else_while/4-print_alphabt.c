d72293d985e27620744c34cea5f6285088e35e00
Graceomole <graceomoleye123@gmail.com>
2022-09-09 18:53:52 -0700

    C code

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: print alphabet in lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+  int letter;
+  for (letter = 'a'; letter <= 'z'; letter++)
+    {
+      if (letter != 'e' && lrtter != 'q')
+	{
+	  putchar(letter);
+	}
+      else
+	{
+	}
+    }
+  putchar('\n');
+  return (0);
+}
