aa1eaa64ec2c1eca6494920bf36496d537ec6ea2
Graceomole <graceomoleye123@gmail.com>
2022-09-09 19:20:30 -0700

    C code

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -13,8 +13,7 @@ int main(void)
   for (x = 0; x < 10; x++)
     {
       printf("%d", x);
-      }
-      putchar('\n');
-      return (0);
-      }
-      
+    }
+  putchar('\n');
+  return (0);
+}
