d40661f73beacafd78bbb265afa6193bf121f77c
Grace Omoleye <105713462+Graceomole@users.noreply.github.com>
2022-09-10 03:43:25 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+
 /**
  *main - Check that prints base 16 all in lowercase alphabates
  *
  *Return:Always 0 (Succesful)
  */
+
 int main(void)
 {
-  char c;
-  for (c = '0'; c <= '9' ; c++)
-    {
-      putchar(c);
-    }
-  for (c = 'a'; c <= 'f' ; c++)
-    {
-      putchar(c);
-    }
-  putchar('\n');
-  return (0);
+char c;
+for (c = '0'; c <= '9' ; c++)
+{
+putchar(c);
+}
+for (c = 'a'; c <= 'f' ; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
 }
