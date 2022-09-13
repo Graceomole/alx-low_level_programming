32bdf6ea00713469d847741578d53c9c25aa2433
Grace Omoleye <105713462+Graceomole@users.noreply.github.com>
2022-09-14 00:59:44 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,14 @@
 
 int main(void)
 {
-system("echo '_putchar\n'");
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
 return (0);
 }
-  
