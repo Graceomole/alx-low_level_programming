8e5b5dcb423a26c4aa35180d3e79c1e006015a58
Grace Omoleye <105713462+Graceomole@users.noreply.github.com>
2022-09-14 03:01:56 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,9 @@
 
 /**
  *
- *  * main - prints the lowercase alphabet in reverse,
- *
- *   * followed by a new line
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * main - prints the lowercase alphabet in reverse, followed by a new line
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
