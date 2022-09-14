2650fc310cee8802289e6655e16fb74c52c0cb13
Grace Omoleye <105713462+Graceomole@users.noreply.github.com>
2022-09-14 03:06:43 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,25 @@
-found it
+#include "main.h"
+
+/**
+ *
+ *  main - prints all the numbers of base 16 in lowercase, followed by a new line
+ * Return: Always 0 (Success)
+ *
+ **/
+
+int main(void)
+
+{
+int n;
+char ch;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
