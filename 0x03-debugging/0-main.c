9c39a546e26849469a9f63f1eadec6ab9979ac8d
Grace Omoleye <105713462+Graceomole@users.noreply.github.com>
2022-09-15 13:51:24 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,10 +5,10 @@
  * Return: 0
  */
 
-int main(void) 
-{  
-int i;  
-i = 0;  
-positive_or_negative(i);  
-return (0); 
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
