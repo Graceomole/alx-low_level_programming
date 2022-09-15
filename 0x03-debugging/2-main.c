fd4f9daf1f6edac6973aa48178d2270743d643e7
Grace Omoleye <105713462+Graceomole@users.noreply.github.com>
2022-09-15 21:06:29 +0100

    Update 2-main.c

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -8,16 +8,13 @@
 
 int main(void)
 {
-        int a, b, c;
-        int largest;
-        
-        a = 972;
-        b = -98;
-        c = 0;
-        
-        largest = largest_number(a, b, c);
-        
-        printf("%d is the largest number\n", largest);
+int a, b, c;
+int largest;
+a = 972;
+b = -98;
+c = 0;
+largest = largest_number(a, b, c);
+printf("%d is the largest number\n", largest);
 
-        return (0);
+return (0);
 }
