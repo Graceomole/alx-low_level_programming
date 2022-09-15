9fb465678b579ddc3498f8a79ef20b973f5121d8
Grace Omoleye <105713462+Graceomole@users.noreply.github.com>
2022-09-15 21:01:52 +0100

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,18 +10,21 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
-if (a > b && b < c)
-{
-largest = a;
-}
-else if (b > a && a > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
-return (largest);
+    int largest;
+  
+    if (a > b && b < c)
+    {
+      
+        largest = a;
+    }
+    else if (b > a && a > c)
+    { 
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+  
+    return (largest);
 }
