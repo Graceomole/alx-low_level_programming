424aec04fef9f43acab2c2178c0e1258c266810f
Grace Omoleye <105713462+Graceomole@users.noreply.github.com>
2022-09-16 02:40:25 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers- print numbers between 0-9'
- * Return: no return'
+ * print_numbers- print numbers between 0-9.
+ * Return: no return.
  */
 
 void print_numbers(void)
