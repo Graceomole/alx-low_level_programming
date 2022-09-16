95172853176aca92a8fb0ff60952324ec20262bd
Grace Omoleye <105713462+Graceomole@users.noreply.github.com>
2022-09-16 01:46:33 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,13 @@
 /**
  * main: checks for uppercase character
  * @c: input character
- * Return: 1 for an uppercase letter, 0 otherwise
- * _isupper: checks if a character is uppercase
+ * Return: 1 for an uppercase letter, 0 otherwise.
+ * _isupper- checks if a character is uppercase.
  */
 
 int _isupper(int c)
 {
+  
 if (c >= 65 && c <= 90)
 {
 return (1);
