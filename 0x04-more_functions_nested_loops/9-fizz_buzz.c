a569b43929263c3f47d7c52ebe6a8af5ab42b9ff
Grace Omoleye <105713462+Graceomole@users.noreply.github.com>
2022-09-16 04:58:42 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,27 +11,29 @@ int main(void)
 int n;
   
 n = 1;
-printf("%d", n);
-for (n = 2; n <= 100; n++)
+for (i = 1; i <= 100; i++)
 {
-if ((n % 3 == 0) && (n % 5 == 0))
-{
-printf(" FizzBuzz");
-}
-else if (n % 3 == 0)
+if (i % 3 == 0 && i % 5 != 0)
 {
 printf(" Fizz");
-else if (n % 5 == 0)
+}
+else if (i % 5 == 0 && i % 3 != 0)
 {
 printf(" Buzz");
 }
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf(" FizzBuzz");
+}else if (i == 1)
+{
+printf("%d", i);
+}
 else
 {
-printf(" %d", n);
+printf(" %d", i);
 }
 }
 printf("\n");
-  
+
 return (0);
 }
-
