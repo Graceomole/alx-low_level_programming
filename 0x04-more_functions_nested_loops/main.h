1d59bc98af04d8d6d68673c0d45fbb21b04aec48
Grace Omoleye <105713462+Graceomole@users.noreply.github.com>
2022-09-16 05:03:30 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -10,3 +13,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
+#endif
