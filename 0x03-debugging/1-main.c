6bb0347ece731afd805048f322d5b6d13202e4b2
Graceomole <graceomoleye123@gmail.com>
2022-09-16 22:11:37 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,19 +8,12 @@ int main(void)
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
-<<<<<<< HEAD
 
 /*while (i < 10);*/
  /*{*/
  /*putchar(i);*/
  /*}*/
 
-=======
-/*while (i < 10);*/
-/*{*/
-/*putchar(i);*/
-/*}*/
->>>>>>> 0933df3d2f923a40f76597f33f164a2883a65a6c
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
