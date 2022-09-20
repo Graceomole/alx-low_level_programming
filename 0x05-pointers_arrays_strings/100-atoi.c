a1236e2057a127d2b932db6c67af8ffdaf5113af
Grace Omoleye <105713462+Graceomole@users.noreply.github.com>
2022-09-20 04:18:39 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,7 +15,7 @@ if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 break;
 if (*(s + count) == '-')
 pn *= -1;
-if ((*(s + count) >= '0') && (*(s + count) <= '9')
+if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 {
 if (size > 0)
 m *= 10;
