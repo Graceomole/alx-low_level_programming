373db841a1012c33d626fa6e061916097fa18fbd
Graceomole <graceomoleye123@gmail.com>
2022-09-19 17:53:46 -0700

    pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,14 @@
 
 /**
  * _strlen - returns the lenght of a string
- * @s - input string
+ * @s: input string
  * Return: length of the string.
  */
 
 int _strlen(char *s)
 {
 int count = 0;
-while(*(s + count) != '\0')	
+while (*(s + count) != '\0')
 count++;
 return (count);
 }
