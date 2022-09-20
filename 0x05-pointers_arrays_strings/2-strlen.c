0e7da39a69c67d4cbd0922d46176da32a7c151be
Graceomole <graceomoleye123@gmail.com>
2022-09-19 17:48:00 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s - input string
+ * Return: length of the string.
+ */
+
+int _strlen(char *s)
+{
+int count = 0;
+while((*(s + count) != '\0')	
+count++;
+return (count);
+}
