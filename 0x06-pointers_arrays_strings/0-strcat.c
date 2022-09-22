1bd90b8e8319a09de32a84187240d68cb6d5b004
Graceomole <graceomoleye123@gmail.com>
2022-09-22 00:49:41 -0700

    strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,10 +8,12 @@
 char *_strcat(char *dest, char *src)
 {
 int count = 0, count2 = 0;
+
 while (*(dest + count) != '\0')
 {
 count++;
 }
+
 while (count2 >= 0)
 {
 *(dest + count) = *(src + count2);
