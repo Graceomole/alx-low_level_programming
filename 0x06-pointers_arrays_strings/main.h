5c15d1a40efe7de4f65328d0858ea02de5ca58eb
Graceomole <graceomoleye123@gmail.com>
2022-09-21 21:20:44 -0700

    strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,4 +11,6 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-char _putchar(char ch);
+char _putchar(char);
+
+#endif
