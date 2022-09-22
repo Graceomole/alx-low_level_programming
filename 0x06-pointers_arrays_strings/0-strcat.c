a45d540c3030df5362f4ed7a5d790fc45787acc7
Graceomole <graceomoleye123@gmail.com>
2022-09-21 20:49:58 -0700

    strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - a function that concanate a string
+ * @dest : first string
+ * @src : second string.
+ * Return: always 0
+ */
+
+int main(void)
+{
+strcat(*dest, *src);
+return (0);
+}
