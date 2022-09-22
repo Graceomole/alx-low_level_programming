40d878f805726c84bd6d936e264306fdd24a3eb9
Graceomole <graceomoleye123@gmail.com>
2022-09-22 01:14:54 -0700

    strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,22 +6,24 @@
  * @src: source.
  * Return: returns the pointer to dest.
  */
- 
-char *_strcat(char *dest, char *src)
-{
 char *_strcat(char *dest, char *src)
 {
 int i, j;
+
 i = 0;
 j = 0;
+
 while (dest[i] != '\0')
 i++;
+
 while (src[j] != '\0')
 {
 dest[i] = src[j];
 j++;
 i++;
 }
+
 dest[i] = '\0';
+
 return (dest);
 }
